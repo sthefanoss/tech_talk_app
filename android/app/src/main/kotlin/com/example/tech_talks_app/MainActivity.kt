bc55c69d3501f193ece286fc11829851871d7f5e
Sthefano Schiavon <sthefanoss@gmail.com>
2021-08-10 19:15:56 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/tech_talks_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tech_talks_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Hello, World!');
+  }
+}
